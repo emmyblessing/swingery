f2e8d698cf23cf85e1fe167b17cd64fc2a3cc0f2
emmyblessing <emmyblessing78@gmail.com>
2023-10-23 23:08:56 +0100

    design updated

---
+++ src/pages/settings/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SettingsPage from '../../components/settings'
+
+const Settings = () => {
+  return (
+    <div>
+      <SettingsPage />
+    </div>
+  )
+}
+
+export default Settings
